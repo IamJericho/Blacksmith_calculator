d7af1d9b7e6f38337f9b778af42d39e39279629f
IamJericho <HerringClinton@gmail.com>
2026-01-06 16:38:51 -0600

    Add files via upload
    one source file

---
+++ blacksmith.js
@@ -0,0 +1,128 @@
+/* ============================================================
+   BLACKSMITH MASTER DATA FILE
+   Single source of truth for ALL craftables
+   Plain JS (no modules)
+   ============================================================ */
+
+   window.BLACKSMITH = {
+    saddles: {
+      regular: {
+        "Allosaurus":{materials:{F:2394,M:422,H:4507},tek:25},
+        "Ankylosaurus":{materials:{F:2158,M:154,H:4008},tek:20},
+        "Araneo":{materials:{K:1345,H:3497,F:1883},tek:15},
+        "Archelon":{materials:{F:8617,H:12208,M:2872,W:3590},tek:75},
+        "Argentavis":{materials:{K:2207,H:5151,F:2722},tek:15},
+        "Armadoggo":{materials:{F:3557,M:474,H:2845,P:79},tek:25},
+        "Arthropluera":{materials:{A:211,N:112,M:423,W:282,F:705,H:1128,O:493},tek:25},
+        "Aurellax":{materials:{F:11357,M:4867,H:9734,P:1622},tek:115},
+  
+        "Baryonyx":{materials:{F:5190,M:370,H:3336},tek:25},
+        "Basilisk":{materials:{A:1170,G:619,M:5852,F:2065,H:3717},tek:135},
+        "Basilosaurus":{materials:{F:3532,M:777,H:2825},tek:35},
+        "Beelzebufo":{materials:{A:74,H:2520,F:1408,W:444},tek:15},
+        "Bison":{materials:{A:1046,H:3836,F:2092,M:697},tek:30},
+        "Brontosaurus":{materials:{F:2297,M:496,H:4346},tek:25},
+  
+        "Carbonemys":{materials:{A:155,H:2636,F:1473},tek:15},
+        "Carcharodontosaurus":{materials:{K:276,H:6913,F:4839,M:2489},tek:65},
+        "Carnotaurus":{materials:{F:2274,M:401,H:4281},tek:25},
+        "Castoroides":{materials:{A:2109,H:4369,T:2711,F:3013,M:1506},tek:50},
+        "Ceratosaurus":{materials:{F:3787,M:280,H:1823},tek:25},
+        "Chalicotherium":{materials:{F:2461,M:615,H:3623},tek:30},
+  
+        "Companion Ammo Box":{materials:{F:227,M:60,H:37},tek:20},
+        "Companion Rucksack":{materials:{F:278,M:9,H:185},tek:20},
+  
+        "Daeodon":{materials:{F:1344,M:173,H:2515},tek:20},
+        "Deinonychus":{materials:{F:2425,W:746,H:4104},tek:15},
+        "Deinosuchus":{materials:{F:2662,M:665,H:5059},tek:30},
+        "Deinotherium":{materials:{A:1968,H:1968,W:4594,F:7482,M:262},tek:25},
+        "Diplodocus":{materials:{F:9545,M:3181,H:13522,W:3977},tek:80},
+        "Dire Bear":{materials:{A:1291,H:3873,F:1678},tek:15},
+        "Doedicurus":{materials:{F:1695,M:77,H:3082,S:231},tek:20},
+        "Dunkleosteus":{materials:{A:1749,H:4374,F:2624,M:1166},tek:40},
+  
+        "Equus":{materials:{F:2066,S:1033,H:3099,W:1097},tek:15},
+        "Fasolasuchus":{materials:{F:2629,M:394,H:1971,W:1314},tek:25},
+  
+        "Gacha":{materials:{K:2241,H:5229,F:2764},tek:15},
+        "Gallimimus":{materials:{F:2408,M:376,H:3613,W:1806},tek:25},
+        "Gasbags":{materials:{K:1885,H:4399,F:2325},tek:15},
+        "Giganotosaurus":{materials:{F:4838,M:1659,H:9401},tek:50},
+        "Gigantoraptor":{materials:{F:2548,M:875,H:4538,W:1911},tek:35},
+  
+        "Helicoprion":{materials:{F:3261,M:978,H:5217},tek:35},
+        "Hyaenodon":{materials:{F:2160,M:278,H:4041},tek:25},
+  
+        "Ichthyosaurus":{materials:{F:795,H:874,W:318,N:127,RM:159},tek:20},
+        "Iguanodon":{materials:{F:669,W:200,H:1071},tek:15},
+  
+        "Karkinos":{materials:{V:1202,FW:3005,F:3381,H:2555},tek:20},
+        "Lymantria":{materials:{K:1399,H:1915,F:1841},tek:15},
+  
+        "Malwyn":{materials:{K:830,H:1038,F:3462,M:2492},tek:65},
+        "Mammoth":{materials:{F:1926,M:137,H:3577},tek:20},
+        "Mantis":{materials:{F:1076,M:502,H:1722},tek:30},
+        "Megalania":{materials:{F:3407,M:713,H:4992},tek:30},
+        "Megaloceros":{materials:{F:1536,M:69,H:2793},tek:20},
+        "Megalosaurus":{materials:{F:2507,M:442,H:4720},tek:25},
+        "Megatherium":{materials:{A:1568,H:5099,F:2039,M:862},tek:35},
+        "Megataptor":{materials:{F:10264,M:2566,H:17963},tek:70},
+        "Mosasaurus":{materials:{A:1943,H:11105,B:1388,F:8328,M:5552},tek:130},
+  
+        "Ossidon":{materials:{F:2687,M:2015,H:5106},tek:60},
+  
+        "Pachy":{materials:{F:955,W:294,H:1617},tek:15},
+        "Pachyrhinosaurus":{materials:{F:1096,W:342,H:1918},tek:15},
+        "Pararacer":{materials:{F:1566,M:142,H:2848},tek:20},
+        "Parasaur":{materials:{F:646,W:194,H:1035},tek:15},
+        "Pelagornis":{materials:{K:1050,H:3222,F:1751},tek:15},
+        "Plesiosaur":{materials:{A:850,H:5231,F:3269,B:523},tek:15},
+        "Pulmonoscorpius":{materials:{F:1395,W:440,H:2496},tek:15},
+        "Pteranodon":{materials:{K:1118,H:3429,F:1863},tek:15},
+  
+        "Quetzal":{materials:{A:1261,H:9462,F:6308,B:1072},tek:15},
+  
+        "Raptor":{materials:{F:826,W:254,H:1399},tek:15},
+        "Ravager":{materials:{F:1908,H:4352,FW:1679},tek:15},
+        "Rex":{materials:{F:2967,M:741,H:5637},tek:30},
+        "Rhynio":{materials:{C:380,H:1904,P:761,F:2539,M:1904},tek:55},
+        "Rock Drake":{materials:{F:3334,M:1852,H:5186,R:1259},tek:55},
+        "Rock Golem":{materials:{F:2015,M:470,H:3628},tek:25},
+        "Roll Rat":{materials:{A:1168,G:618,M:5842,F:2062,H:3711},tek:135},
+  
+        "Sabertooth":{materials:{F:2066,M:266,H:3866},tek:25},
+        "Sarco":{materials:{A:284,H:3272,F:1066},tek:15},
+        "Snow Owl":{materials:{K:2047,H:4777,F:2525},tek:15},
+        "Solwyn":{materials:{C:1902,M:792,F:3567,O:1268},tek:35},
+        "Spino":{materials:{K:587,H:4961,F:2611,B:326},tek:15},
+        "Stegosaurus":{materials:{F:1498,W:476,H:2723},tek:15},
+  
+        "Tapejara":{materials:{F:2438,M:609,H:3521,W:1625},tek:30},
+        "Terror Bird":{materials:{F:1013,W:311,H:1714},tek:15},
+        "Therizinosaurus":{materials:{F:1970,M:677,H:3510,W:1478},tek:30},
+        "Thorny Dragon":{materials:{Y:1199,H:3810,W:1764,F:2116,M:493},tek:25},
+        "Thylacoleo":{materials:{F:2045,M:263,H:3826},tek:25},
+        "Trike":{materials:{F:1052,W:329,H:1842},tek:15},
+        "Tusoteuthis":{materials:{A:701,H:4991,F:12401,M:467},tek:25},
+  
+        "Velonasaur":{materials:{K:2290,H:5344,F:2825},tek:15},
+  
+        "Woolly Rhino":{materials:{A:1417,H:3543,F:1842,M:850},tek:35},
+        "Xiphactinus":{materials:{A:753,H:4371,F:2336,M:226},tek:20},
+        "Yi Ling":{materials:{F:508,W:156,H:860},tek:15},
+        "Yutyrannus":{materials:{F:5548,M:2140,H:6737,B:1981},tek:60}
+      },
+  
+      platform: {
+        "Brontosaurus Platform":{materials:{A:675,H:8254,B:1876,F:4877,M:1350},tek:45},
+        "Dreadnoughtus Platform":{materials:{A:4533,H:18133,B:3777,F:12088,M:9822},tek:215},
+        "Mosasaurus Platform":{materials:{A:2707,H:14438,B:4812,F:10829,M:18048},tek:380},
+        "Paraceratherium Platform":{materials:{A:359,H:4597,B:646,F:2873,M:1005},tek:40},
+        "Plesiosaur Platform":{materials:{A:431,H:5335,B:1216,F:3177,M:878},tek:35},
+        "Quetzal Platform":{materials:{A:1641,H:8483,B:3010,F:5473,M:2462},tek:65},
+        "Shastasaurus Platform":{materials:{A:2605,E:1954,B:3256,C:4559,M:19541},tek:410}
+      }
+    }
+  };
+  
