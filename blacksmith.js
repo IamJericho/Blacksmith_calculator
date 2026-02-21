c32977dba614067a99deaee2bf08ee791ee33f33
IamJericho <HerringClinton@gmail.com>
2026-02-21 11:16:17 -0600

    Update blacksmith.js
    Updated pricing on pump shotgun

--- blacksmith.js
+++ blacksmith.js
@@ -325,7 +325,7 @@ window.BLACKSMITH = {
         "Longneck Rifle": { materials: { H: 697, RE: 139, M: 2649, W: 557, D: 220 }, tek: 175 },
         "Pick": { materials: { H: 294, RE: 29, M: 29, W: 29, D: 167 }, tek: 60 },
         "Pike": { materials: { H: 645, RE: 32, M: 322, W: 322, D: 180 }, tek: 60 },
-        "Pump Shotgun": { materials: { A: 2098, P: 2564, M: 1398, RE: 466, D: 1873 }, tek: 555 },
+        "Pump Shotgun": { materials: { A: 2098, P: 2564, M: 1398, RE: 466, D: 1873 }, tek: 750 },
         "Sword": { materials: { H: 571, RE: 38, M: 1903, W: 38, D: 363, }, tek: 60 }
       },/* 
       tools: {},*/
